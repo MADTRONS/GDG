b9810ad34e21d30ada495ec0c6ed0458032deac9
mk sinha <mksinha77756@gmail.com>
2025-12-14 13:03:01 +0530

    Update Assistant class to improve Hindi language support and modify session initialization

--- agent.py
+++ agent.py
@@ -1,42 +1,72 @@
 from dotenv import load_dotenv
-
-from livekit import agents, rtc
-from livekit.agents import AgentServer, AgentSession, Agent, room_io
+# from prompts import AGENT_INSTRUCTION, SESSION_INSTRUCTION
+from livekit import agents
+from livekit.agents import AgentSession, Agent, RoomInputOptions
 from livekit.plugins import (
     openai,
     noise_cancellation,
 )
+# from mcp_client import MCPServerSse
+# from mcp_client.agent_tools import MCPToolsIntegration
+import os
+# from tools import open_url
+from livekit.plugins import tavus
+load_dotenv()
 
-load_dotenv(".env.local")
 
 class Assistant(Agent):
     def __init__(self) -> None:
-        super().__init__(instructions="You are a helpful voice AI assistant.")
+        super().__init__(instructions="you are a helpful assistant.talk like a friendly human. in hindi",
+                         tools=[],)
 
-server = AgentServer()
 
-@server.rtc_session()
-async def my_agent(ctx: agents.JobContext):
+async def entrypoint(ctx: agents.JobContext):
     session = AgentSession(
         llm=openai.realtime.RealtimeModel(
-            voice="coral"
+            voice="shimmer"
         )
     )
 
+    # mcp_server = MCPServerSse(
+    #     params={"url": os.environ.get("N8N_MCP_SERVER_URL")},
+    #     cache_tools_list=True,
+    #     name="SSE MCP Server"
+    # )
+
+    # agent = await MCPToolsIntegration.create_agent_with_tools(
+    #     agent_class=Assistant,
+    #     mcp_servers=[mcp_server]
+    # )
+    
+    agent = Assistant()
+
+    # avatar = tavus.AvatarSession(
+    #   replica_id=os.environ.get("REPLICA_ID"),  
+    #   persona_id=os.environ.get("PERSONA_ID"),  
+    #   api_key=os.environ.get("TAVUS_API_KEY"),
+    # )
+
+    # Start the avatar and wait for it to join
+    # await avatar.start(session, room=ctx.room)
+
+
     await session.start(
         room=ctx.room,
-        agent=Assistant(),
-        room_options=room_io.RoomOptions(
-            audio_input=room_io.AudioInputOptions(
-                noise_cancellation=lambda params: noise_cancellation.BVCTelephony() if params.participant.kind == rtc.ParticipantKind.PARTICIPANT_KIND_SIP else noise_cancellation.BVC(),
-            ),
+        agent=agent,
+        room_input_options=RoomInputOptions(
+            # LiveKit Cloud enhanced noise cancellation
+            # - If self-hosting, omit this parameter
+            # - For telephony applications, use `BVCTelephony` for best results
+            noise_cancellation=noise_cancellation.BVC(),
         ),
     )
 
+    await ctx.connect()
+
     await session.generate_reply(
-        instructions="Greet the user and offer your assistance. You should start by speaking in HINDI."
+        instructions="you are a helpful assistant.talk like a friendly human. in hindi",
     )
 
 
 if __name__ == "__main__":
-    agents.cli.run_app(server)
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
